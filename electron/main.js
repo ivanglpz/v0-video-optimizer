21da92b8bf4fd5990625cb9c70e9d1847312bc97
ivanglpz <miltonivan117@gmail.com>
2025-10-09 15:31:49 -0600

    update express server electron app

--- electron/main.js
+++ electron/main.js
@@ -1,16 +1,27 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
+const express = require("express");
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, "preload.js"), // Optional, for security
+      preload: path.join(__dirname, "preload.js"), // si lo tienes
     },
   });
 
-  win.loadFile(path.join(__dirname, "../out/index.html"));
+  // Servidor Express
+  const server = express();
+  const port = 3000;
+
+  server.use(express.static(path.join(__dirname, "../out")));
+
+  server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+    // Carga la app de Next export en Electron
+    win.loadURL(`http://localhost:${port}`);
+  });
 }
 
 app.whenReady().then(createWindow);
